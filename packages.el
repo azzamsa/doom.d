64a0803871e3536805c3e0403120fbe30f4d707c
azzamsa <me@azzamsa.com>
2022-09-15 19:16:05 +0700

    fix: `fish-mode` is `+fish` in `sh` module

--- packages.el
+++ packages.el
@@ -4,6 +4,5 @@
 (package! evil-colemak-basics)
 (package! super-save)
 (package! org-cliplink)
-(package! fish-mode)
 (package! lsp-tailwindcss :recipe (:host github :repo "merrickluo/lsp-tailwindcss"))
 (package! lsp-pyright :recipe(:type git :flavor melpa :host github :repo "emacs-lsp/lsp-pyright"))
